931f0bf010544e46669f59cf63b9fc26059121f2
axdiaz <axeldiaz89@gmail.com>
2020-06-10 07:39:41 -0300

    borre un archivo basura, acomode la screenPaso1- TODOS LOS TEST PASAN

--- __tests__/ScreenPaso1.spec.js
+++ __tests__/ScreenPaso1.spec.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {render} from 'react-native-testing-library';
 import ScreenPaso1 from '../screens/screenPaso1/ScreenPaso1';
 
+
 describe('ScreenPaso1', () => {
   it('renders the correct message', () => {
     const {queryByText} = render(<ScreenPaso1 />);
--- screens/screenPaso1/ScreenPaso1.js
+++ screens/screenPaso1/ScreenPaso1.js
@@ -1,58 +1,17 @@
-import React, {useState} from 'react';
-import {View, Button, Platform, Text} from 'react-native';
-import DateTimePicker from '@react-native-community/datetimepicker';
+import React from 'react';
+import { View, Text, TextInput } from 'react-native';
 
-const ScreenPaso1 = () => {
-  const [date, setDate] = useState(new Date(1598051730000));
-  const [mode, setMode] = useState('date');
-  const [show, setShow] = useState(false);
-
-  const onChange = (event, selectedDate) => {
-    const currentDate = selectedDate || date;
-    setShow(Platform.OS === 'ios');
-    setDate(currentDate);
-  };
-
-  const showMode = (currentMode) => {
-    setShow(true);
-    setMode(currentMode);
-  };
-
-  const showDatepicker = () => {
-    showMode('date');
-  };
-
-  const showTimepicker = () => {
-    showMode('time');
-  };
-
-  console.log(date);
-  console.log(mode);
-  console.log(show);
-  const cate = "";
-
+export default function ScreenPaso1() {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <View>
-        <Button onPress={showDatepicker} title="Show date picker!" />
-      </View>
+    <View style={{ flex:1, justifyContent: 'center', alignItems: 'center'}}>
 
-      {show && (
-        <DateTimePicker
-          testID="dateTimePicker"
-          value={date}
-          mode={mode}
-          is24Hour={true}
-          display="default"
-          onChange={onChange}
-        />
-      )}
+      <Text>Soy la pagina ScreenPaso1</Text>
 
-      <View>
-        <Text>{cate ? cate : null}</Text>
-      </View>
+      <TextInput
+        placeholder="Fecha"
+        >
+
+      </TextInput>
     </View>
-  );
-};
-
-export default ScreenPaso1;
+  )
+}
