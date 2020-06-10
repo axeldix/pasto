195c9e040fa407910d2fa2c96cec6c706d997a96
axdiaz <axeldiaz89@gmail.com>
2020-06-10 10:25:58 -0300

    globalStyles

---
+++ styles/globalStyles.js
@@ -0,0 +1,22 @@
+import {StyleSheet} from 'react-native';
+
+export const globalStyles = StyleSheet.create({
+
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'stretch',
+    margin: 25
+  },
+
+  screenTitle: {
+    fontSize: 22,
+  },
+
+  textInput: {
+    paddingTop: 10,
+    borderBottomWidth: 1,
+    color: 'black',
+    fontSize: 17,
+  },
+});
