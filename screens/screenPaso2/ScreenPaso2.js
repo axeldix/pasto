7eacb70f673f8440584419c621e8835b746f262f
axdiaz <axeldiaz89@gmail.com>
2020-07-19 14:04:12 -0300

    agregue keyboard type numeric

--- screens/screenPaso2/ScreenPaso2.js
+++ screens/screenPaso2/ScreenPaso2.js
@@ -53,24 +53,28 @@ export default function ScreenPaso1() {
           placeholder="Superficie de parcela (Ha.)"
           style={globalStyles.textInput}
           onChangeText={handleSuperficie}
+          keyboardType="numeric"
         />
 
         <TextInput
           placeholder="Porcentaje de Materia Seca"
           style={globalStyles.textInput}
           onChangeText={handlePorcentajeMateriaSeca}
+          keyboardType="numeric"
         />
 
         <TextInput
           placeholder="Cantidad de Vacas"
           style={globalStyles.textInput}
           onChangeText={handleCantidadVacas}
+          keyboardType="numeric"
         />
 
         <TextInput
           placeholder="Area del Cuadrante"
           style={globalStyles.textInput}
           onChangeText={handleAreaCuadrante}
+          keyboardType="numeric"
         />
       </View>
     </TouchableWithoutFeedback>
